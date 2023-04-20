e2b51c54a7577ff7935681128127ac3a3d4514f3
shinsam <shin321gg@gmail.com>
2023-04-20 15:46:04 +0900

    1

--- Java20600/src/s0420/Exam02.java
+++ Java20600/src/s0420/Exam02.java
@@ -4,7 +4,7 @@ public class Exam02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("TEST2222");
+		System.out.println("TEST22222");
 	}
 
 }
