fb58413951400d8ec0cde589e547ec2400f9209a
shinsam <shin321gg@gmail.com>
2023-04-20 14:04:32 +0900

    code

--- Java20600/src/s0420/Exam01.java
+++ Java20600/src/s0420/Exam01.java
@@ -3,5 +3,8 @@ package s0420;
 public class Exam01 {
 	public static void main(String[] args) {
 		System.out.println("QQQ");
+		System.out.println("QQQ");
+		System.out.println("QQQ");
+		System.out.println("QQQ");
 	}
 }
